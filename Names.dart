ac9aaebf122cfe5e96ca223459fc1106c3881f7c
AbdulQadir888 <abdulqadir.jamil888@gmail.com>
2024-01-28 22:58:23 +0500

    first time commit message

---
+++ Names.dart
@@ -0,0 +1,9 @@
+void main() {
+  // List of names
+  List<String> names = ["Alice", "Bob", "Charlie", "David", "Eva"];
+
+  // Print all names
+  for (String name in names) {
+    print(name);
+  }
+}
